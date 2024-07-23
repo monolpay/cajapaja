e32acac453c131116a6e52792ad5a1e2826ecd07
monolpay <jan1nap@seznam.cz>
2024-07-23 13:35:45 +0200

    fixed github

---
+++ mail.js
@@ -0,0 +1,13 @@
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "jan.naprstek@gmail.com",
+    Password : "19C6D467190B47C56D58682AC59A5E08BDD9",
+    To : 'jan1nap@seznam.cz',
+    From : "jan.naprstek@kcentrum.eu",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
