313bfbe1a7835f1b4aea96c8cce1012036faf8de
monolpay <jan1nap@seznam.cz>
2024-07-26 15:36:55 +0200

    ha,burger menu nice and working

--- menu.js
+++ menu.js
@@ -1,3 +1,13 @@
-function menu() {
+let opened = false
+
+function menu() { 
+    if(opened == false) {
+        document.getElementById("menubar").style="animation: menuOut 1s both"
+        opened = true
+    }
+    else if(opened){
+        document.getElementById("menubar").style="animation: menuIn 1s both"
+        opened = false
+    }
     
 }
