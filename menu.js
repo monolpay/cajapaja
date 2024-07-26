e99f6de1633bc713b56c90c16fa8ff5ead8f5fae
monolpay <jan1nap@seznam.cz>
2024-07-26 18:35:45 +0200

    another hotfix

--- menu.js
+++ menu.js
@@ -4,10 +4,22 @@ function menu() {
     if(opened == false) {
         document.getElementById("menubar").style="animation: menuOut 1s both"
         opened = true
+        switchZIndex()
     }
     else if(opened){
         document.getElementById("menubar").style="animation: menuIn 1s both"
         opened = false
+        setTimeout(switchZIndex, 1000)
     }
     
 }
+
+function switchZIndex(){
+    if(opened == false) {
+        document.getElementById("objednavka").style="z-index: 25;"
+    }
+    else if(opened){
+        document.getElementById("objednavka").style="z-index: -1;"
+    }
+}
+
